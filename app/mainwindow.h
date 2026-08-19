b6fb120348bcf522ab0946445a01da0d668863b4
zamuuk13-sketch <zamuuk13@gmail.com>
2026-08-19 16:39:18 -0300

    Rebuild Nexora professional workspace architecture

--- app/mainwindow.h
+++ app/mainwindow.h
@@ -4,6 +4,9 @@
 #include <QPlainTextEdit>
 #include <QComboBox>
 #include <QLabel>
+#include <QStackedWidget>
+#include <QLineEdit>
+#include <QPushButton>
 #include "bridge/BridgeServer.h"
 #include "agent.h"
 
@@ -15,14 +18,25 @@ private slots:
     void sendPrompt();
     void addAI();
     void bridgeState(bool connected);
+    void showHome();
+    void showWorkspace();
+    void createProject();
+    void configureFirstAI();
 private:
     void buildUi();
+    void buildHome();
+    void buildWorkspace();
     void applyStyle();
     QList<AIProfile> m_profiles;
     BridgeServer m_bridge;
     Agent m_agent;
-    QComboBox *m_aiSelector = nullptr;
-    QPlainTextEdit *m_chat = nullptr;
-    QPlainTextEdit *m_prompt = nullptr;
-    QLabel *m_status = nullptr;
+    QStackedWidget *m_pages=nullptr;
+    QWidget *m_home=nullptr;
+    QWidget *m_workspace=nullptr;
+    QComboBox *m_aiSelector=nullptr;
+    QPlainTextEdit *m_chat=nullptr;
+    QPlainTextEdit *m_prompt=nullptr;
+    QLabel *m_status=nullptr;
+    QLabel *m_projectTitle=nullptr;
+    QLineEdit *m_projectName=nullptr;
 };
