9be7ffeea622df184024b633f2fc85d2fc4d66a3
zamuuk13-sketch <zamuuk13@gmail.com>
2026-08-19 14:37:33 -0300

    Add Nexora tool registry

---
+++ app/tools/ToolRegistry.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <QStringList>
+
+namespace NexoraTools {
+inline QStringList names() {
+    return {"inspect_project","inspect_instance","create_instance","delete_instance","clone_instance","get_property","set_property","create_script","edit_script","delete_script","create_model","create_ui","create_npc","create_animation","run_test","read_output","select"};
+}
+}
